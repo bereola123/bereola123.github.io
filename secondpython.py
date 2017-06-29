737e133f31d894a76772e4d4032aa53dafb30950
Bereola-Carson Daijon <daijon.bereolacarson@infotech.hamptonu.edu>
2017-06-29 16:47:27 -0400

    python 6/29

--- secondpython.py
+++ secondpython.py
@@ -1,10 +1,11 @@
+#first lesson 06/28/17
+
 '''multiple
 line
 comment'''
 #single line comment
-
-num = raw_input('Enter number')
-num = int(num)
+'''num = raw_input('Enter number')
+num = int(num)#convert to integer
 #for-loop
 for i in range (1, num + 1):
     print i
@@ -12,4 +13,103 @@ for i in range (1, num + 1):
     n = 1
     while n <= num :
         print n
-        n = n + 1
+        n = n + 1 #i++ will not work'''
+'''for i in range (0 , 101, 10) : #(start, stop + 1, multiple)
+    print i'''
+'''for i in range(10, 0, -1):
+    print i'''
+'''for i in range (100, -5, -5) :
+    print i'''
+
+#second lesson 06/29/17
+#groceries = ["Eggs", "Milk", "Butter", "Onion", "Apples"]
+'''print groceries[4]
+
+print groceries[1:4]
+
+print groceries [:4]
+
+print groceries [3:]
+
+print groceries [0:5:2]'''
+
+'''print len(groceries)
+
+groceries.index('Apples')'''
+
+'''
+>>> name.lower
+<built-in method lower of str object at 0x0000000003D417E0>
+>>> name.lower()
+'jennifer'
+>>> name.upper()
+'JENNIFER'
+>>> name.capitalize()
+'Jennifer'
+>>> sentence = "what will I have for dinner?"
+>>> sentence
+'what will I have for dinner?'
+>>> sentence.capitalize
+<built-in method capitalize of str object at 0x0000000003C78830>
+>>> sentence.capitalize()
+'What will i have for dinner?'
+>>> name[7]
+'r'
+>>> name.replace('e', 'a')
+'Jannifar'
+>>> name.replace('i', 'i'*5)
+'Jenniiiiifer'
+>>> name.replace('e', 'a', 1)
+'Jannifer'
+'''
+'''for i in groceries :
+    print i'''
+
+'''#define a function
+
+def say_hello( ) :
+    print 'Hello!'
+    print 'It\'s Thursday!'
+
+def print_info(text):
+    print 'Value is ' + text
+
+#define cal_power
+def cal_power(base, power):
+    answer = 1
+    for i in range(power):
+        answer *= base
+    return answer
+    
+say_hello()
+print_info('June is almost over!')
+print_info('What is your plan for July 4th?')
+
+print cal_power(4, 2)
+print cal_power(3, 5)
+
+b = raw_input('Enter base: ')
+p = raw_input('Enter power: ')
+print cal_power(int(b), int(p))
+
+import math
+
+print math.pow(4,5)'''
+
+'''groceries_by_price = {
+    'Eggs' : 2.59,
+    'Milk' : 3.19,
+    'Butter' : 2.69,
+    'Yogurt' : 3.19
+    }
+print groceries_by_price['Eggs']
+for item in groceries_by_price:
+    print item + " " + str(groceries_by_price[item])'''
+
+def create_box(width, height) :
+    for i in range(height):
+        print '#' * width
+w = raw_input('Enter width: ')
+h = raw_input('Enter heigh: ')
+
+create_box(int(w), int(h))
---
+++ third.py
@@ -0,0 +1,612 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:53:40) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+1
+2
+3
+4
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+1
+2
+3
+4
+5
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+>>> 
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+0
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+-1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+0
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+100
+95
+90
+85
+80
+75
+70
+65
+60
+55
+50
+45
+40
+35
+30
+25
+20
+15
+10
+5
+0
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+['Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+['Eggs', 'Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+['Apples']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+['Onion', 'Apples']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 26, in <module>
+    print groceries[0,2,4]
+TypeError: list indices must be integers, not tuple
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 26, in <module>
+    print groceries[0, 2, 4]
+TypeError: list indices must be integers, not tuple
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+['Onion', 'Apples']
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Apples
+['Milk', 'Butter', 'Onion']
+['Eggs', 'Milk', 'Butter', 'Onion']
+['Onion', 'Apples']
+['Eggs', 'Butter', 'Apples']
+>>> 
+>>> 
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 36, in <module>
+    len(groceries)
+NameError: name 'groceries' is not defined
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 36, in <module>
+    print len(groceries)
+NameError: name 'groceries' is not defined
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+5
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+5
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Eggs
+Milk
+Butter
+Onion
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Eggs
+Milk
+Butter
+Onion
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Eggs
+Milk
+Butter
+Onion
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Eggs
+Milk
+Butter
+Onion
+Apples
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Eggs
+Milk
+Butter
+Onion
+Apples
+>>> groceries.sorted
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    groceries.sorted
+AttributeError: 'list' object has no attribute 'sorted'
+>>> groceries.sorted()
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    groceries.sorted()
+AttributeError: 'list' object has no attribute 'sorted'
+>>> groceries
+['Apples', 'Onion', 'Butter', 'Milk', 'Eggs']
+>>> groceries.sort()
+>>> groceries.reverse()
+>>> groceries
+['Onion', 'Milk', 'Eggs', 'Butter', 'Apples']
+>>> groceries.sort(reverse=True)
+>>> groceries
+['Onion', 'Milk', 'Eggs', 'Butter', 'Apples']
+'
+>>> groceries.append('Asparagus')
+>>> groceries
+['Onion', 'Milk', 'Eggs', 'Butter', 'Apples', 'Asparagus']
+>>> groceries.extend(['Ice Cream', 'Bananas'])
+>>> groceries
+['Onion', 'Milk', 'Eggs', 'Butter', 'Apples', 'Asparagus', 'Ice Cream', 'Bananas']
+>>> groceries.insert(2, 'Oranges')
+>>> groceries
+['Onion', 'Milk', 'Oranges', 'Eggs', 'Butter', 'Apples', 'Asparagus', 'Ice Cream', 'Bananas']
+>>> groceries.remove('Apples')
+>>> groceries
+['Onion', 'Milk', 'Oranges', 'Eggs', 'Butter', 'Asparagus', 'Ice Cream', 'Bananas']
+>>> name = 'Jennifer'
+>>> len(name)
+8
+>>> name.lower
+<built-in method lower of str object at 0x0000000003D417E0>
+>>> name.lower()
+'jennifer'
+>>> name.upper()
+'JENNIFER'
+>>> name.capitalize()
+'Jennifer'
+>>> sentence = "what will I have for dinner?"
+>>> sentence
+'what will I have for dinner?'
+>>> sentence.capitalize
+<built-in method capitalize of str object at 0x0000000003C78830>
+>>> sentence.capitalize()
+'What will i have for dinner?'
+>>> name[7]
+'r'
+>>> name.replace('e', 'a')
+'Jannifar'
+>>> name.replace('i', 'i'*5)
+'Jenniiiiifer'
+>>> name.replace('e', 'a', 1)
+'Jannifer'
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+
+Value is June is almost over!
+Value is What is your plan for July 4th?
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 79, in <module>
+    print cal_power(4, 2)
+NameError: name 'cal_power' is not defined
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+8
+15
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 84, in <module>
+    print cal_power(4, 2)
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 77, in cal_power
+    result *= base
+UnboundLocalError: local variable 'result' referenced before assignment
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 85, in <module>
+    print cal_power(4, 2)
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 79, in cal_power
+    return answer
+NameError: global name 'answer' is not defined
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 85, in <module>
+    print cal_power(4, 2)
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 78, in cal_power
+    answer *= base
+UnboundLocalError: local variable 'answer' referenced before assignment
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+16
+243
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+16
+243
+Enter base: 4>>> name.lower
+<built-in method lower of str object at 0x0000000003D417E0>
+>>> name.lower()
+'jennifer'
+>>> name.upper()
+'JENNIFER'
+>>> name.capitalize()
+'Jennifer'
+>>> sentence = "what will I have for dinner?"
+>>> sentence
+'what will I have for dinner?'
+>>> sentence.capitalize
+<built-in method capitalize of str object at 0x0000000003C78830>
+>>> sentence.capitalize()
+'What will i have for dinner?'
+>>> name[7]
+'r'
+>>> name.replace('e', 'a')
+'Jannifar'
+>>> name.replace('i', 'i'*5)
+'Jenniiiiifer'
+>>> 4
+Enter power: 
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 90, in <module>
+    print cal_power(int(b), int(p))
+ValueError: invalid literal for int() with base 10: '4>>> name.lower'
+>>> 
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+16
+243
+Enter base: 4
+Enter power: 3
+64
+>>> import math
+>>> math.pow
+<built-in function pow>
+>>> math.pow(4,2)
+16.0
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+16
+243
+Enter base: 5
+Enter power: 2
+25
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 95, in <module>
+    print math.pow(5,10)
+NameError: name 'math' is not defined
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Hello!
+It's Thursday!
+Value is June is almost over!
+Value is What is your plan for July 4th?
+16
+243
+Enter base: 5
+Enter power: 2
+25
+1024.0
+>>> import.random
+SyntaxError: invalid syntax
+>>> import random
+>>> random.randInt(1,5)
+
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    random.randInt(1,5)
+AttributeError: 'module' object has no attribute 'randInt'
+>>> random.randInt(1,3)
+
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    random.randInt(1,3)
+AttributeError: 'module' object has no attribute 'randInt'
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+{'Butter': 2.69, 'Yogurt': 3.19, 'Eggs': 2.59, 'Milk': 3.19}
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+2.59
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+2.59
+Butter
+Yogurt
+Eggs
+Milk
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Butter
+Yogurt
+Eggs
+Milk
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+2.59
+Butter
+Yogurt
+Eggs
+Milk
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+2.59
+
+Traceback (most recent call last):
+  File "C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py", line 107, in <module>
+    print item + " " + groceries_by_price[item]
+TypeError: cannot concatenate 'str' and 'float' objects
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+2.59
+Butter 2.69
+Yogurt 3.19
+Eggs 2.59
+Milk 3.19
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Enter width: 8
+Enter heigh: 5
+########
+########
+########
+########
+########
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Enter width: 5
+Enter heigh: 5
+#####
+#####
+#####
+#####
+#####
+>>> 
+== RESTART: C:\Users\daijon.bereolacarson\Desktop\javatutor\secondpython.py ==
+Enter width: 20
+Enter heigh: 10
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+####################
+>>> 
